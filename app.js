5f63d77293823beeb110da77e7def5db019e484b
Bones <ahansen1335@gmail.com>
2022-01-04 08:43:47 +0100

    buttons created for desired grid sizes

--- app.js
+++ app.js
@@ -1,17 +1,44 @@
 const gridContainer = document.querySelector('#gridContainer');
 
-function grid(){
-    for (let i = 0; i < (16*16); i++) {
+function grid(num) {
+    gridContainer.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${num}, 1fr)`;
+    for (let i = 0; i < (num**2); i++) {
         const gridDivs = document.createElement('div');
             gridDivs.classList.add('gridDivs');
-            gridDivs.textContent = '';
+
         
         gridContainer.appendChild(gridDivs);
-    };
-      
+    }; 
 };
 
-grid();
+grid(100);
+
+const button16 = document.querySelector('#sixteen');
+    button16.addEventListener('click', () => {
+        grid(16);
+        clear()
+    });
+
+const button32 = document.querySelector('#thirtyTwo');
+    button32.addEventListener('click', () => {
+        grid(32);
+        clear();
+    });
+
+const button64 = document.querySelector('#sixtyFour');
+    button64.addEventListener('click', () => {
+        grid(64);
+        clear();
+    });
+
+const buttonCustom = document.querySelector('#custom');
+    buttonCustom.addEventListener('click', () => {
+        let num = prompt('Input desired grid size upto 100:')
+        grid(num);
+        clear();
+    });
+
 
 const gridDivsColor = document.querySelectorAll('.gridDivs');
 
@@ -25,11 +52,6 @@ function addColor() {
 
 addColor();
 
-const clearButton = document.querySelector('#clear');
-    clearButton.addEventListener('click', () => {
-        clear()
-    });
-
 function clear() {
     for (let i = 0; i < gridDivsColor.length; i++) {
         const classes = gridDivsColor[i].getAttribute('class');
@@ -38,6 +60,11 @@ function clear() {
         } 
     };
 };
+
+const clearButton = document.querySelector('#clear');
+    clearButton.addEventListener('click', () => {
+        clear()
+    });
 
 // test function
 function sole() {
